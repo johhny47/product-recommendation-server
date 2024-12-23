20c8a496f08a2a6e321286039517e879fb6df10a
johhny47 <mdjohnny063@gmail.com>
2024-12-23 13:30:11 +0600

    get queries update

--- index.js
+++ index.js
@@ -98,6 +98,13 @@ async function run() {
       const result = await ProductCollection.findOne(query);
       res.send(result)
     })
+    app.get('/queries/update/:id',async(req,res)=>{
+      const id = req.params.id
+      const query = { _id: new ObjectId(id)};
+      const result = await ProductCollection.findOne(query);
+      res.send(result)
+    })
+
    
     
    
