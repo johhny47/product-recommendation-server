70bb2c17c800de0ac3cd824e2cc69d318757298f
johhny47 <mdjohnny063@gmail.com>
2026-03-30 10:20:04 +0600

    add test endpoint

--- index.js
+++ index.js
@@ -268,8 +268,8 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/',(req,res)=>{
-    res.send('Hello from server sid')
+app.get('/test',(req,res)=>{
+    res.send('Hello from server side')
 })
 
 
